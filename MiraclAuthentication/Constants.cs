f6a0f81cccd6cf9f5ed11a9db5c0c7f91c5f8f33
Petya Koleva <petya.koleva@miracl.com>
2018-12-04 14:31:12 +0200

    Add MPinID and HashMPinID

--- MiraclAuthentication/Constants.cs
+++ MiraclAuthentication/Constants.cs
@@ -14,12 +14,14 @@ namespace Miracl
         internal const string RefreshToken = "refresh_token";
         internal const string ExpiresAt = "expires_at";
         internal const string AccessToken = "access_token";
-        internal const string Scope = "openid profile email dvs";
+        internal const string Scope = "openid profile email dvs mpin_id hash_mpin_id";
         internal const string PullEndpoint = "/activate/pull";
         internal const string ActivateEndpoint = "/activate/user";
         internal const string GetIdentityInfoEndpoint = "/activate/check";
         internal const string ActivateInitiateEndpoint = "/activate/initiate";
         internal const string UserIdClaim = "sub";
         internal const string EmailClaim = "email";
+        internal const string MPinIDClaim = "mpin_id";
+        internal const string HashMPinIDClaim = "hash_mpin_id";
     }
 }

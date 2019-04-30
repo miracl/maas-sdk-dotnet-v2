3e253293cccdf0271c4b6644f6183c5240bf79ca
Petya Koleva <petya.koleva@miracl.com>
2019-04-30 10:25:08 +0300

    Add authtoken and authzurl endpoints

---
+++ MiraclDvsSigningApp/Controllers/authzurlController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiraclDvsSigningApp.Controllers
+{
+    public class authzurlController : Controller
+    {
+        [HttpPost]
+        public async Task<ActionResult> Index()
+        {
+            string url = Request.Url.ToString().Replace(Request.Url.Segments[Request.Url.Segments.Length - 1], "");
+            return Json(new { authorizeURL = await HomeController.GetUrl(url) });
+        }
+    }
+}

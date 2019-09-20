9fbd9832cab906778dd48b2de27b84afd20bd2d6
Petya Koleva <petya.koleva@miracl.com>
2019-09-20 12:38:02 +0300

    Remove the old PV modes from the code and samples

--- MiraclAuthentication/Constants.cs
+++ MiraclAuthentication/Constants.cs
@@ -15,9 +15,7 @@ namespace Miracl
         internal const string ExpiresAt = "expires_at";
         internal const string AccessToken = "access_token";
         internal const string Scope = "openid profile email dvs mpin_id hash_mpin_id";
-        internal const string PullEndpoint = "/activate/pull";
         internal const string ActivateEndpoint = "/activate/user";
-        internal const string GetIdentityInfoEndpoint = "/activate/check";
         internal const string ActivateInitiateEndpoint = "/activate/initiate";
         internal const string UserIdClaim = "sub";
         internal const string EmailClaim = "email";
